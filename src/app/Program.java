331a61e0e279062cd8fa7f3e45b540b81b2b593b
Renan Caciolatto <renancaciolatto@hotmail.com>
2023-09-02 20:39:06 -0300

    Solução ruim

--- src/app/Program.java
+++ src/app/Program.java
@@ -1,5 +1,46 @@
 package app;
 
-public class Program {
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.Scanner;
 
+import model.entities.Reservation;
+
+public class Program {
+	public static void main(String[] args) throws ParseException {
+		Scanner sc = new Scanner(System.in);
+		SimpleDateFormat sdf = new SimpleDateFormat("dd/MM/yyyy");
+		
+		System.out.print("Numero do quarto: ");
+		int numeroQuarto = sc.nextInt();
+		System.out.print("Data de Check-In: (dd/mm/yyyy) ");
+		Date checkIn = sdf.parse(sc.next());
+		System.out.print("Data de Check-Out: (dd/mm/yyyy) ");
+		Date checkOut = sdf.parse(sc.next());
+		 
+		if(!checkOut.after(checkIn)) {
+			System.out.println("A data de checkout deve ser posterior a de checkin...");
+		}
+		else {
+			Reservation reserva = new Reservation(numeroQuarto, checkIn, checkOut);
+			System.out.println("Reserva: " + reserva);
+			
+			System.out.println("\nEntre com os dados para atualizar sua reserva: ");
+			System.out.print("Data de Check-In: (dd/mm/yyyy) ");
+			checkIn = sdf.parse(sc.next());
+			System.out.print("Data de Check-Out: (dd/mm/yyyy) ");
+			checkOut = sdf.parse(sc.next());
+			
+			String erro = reserva.atualizarDatas(checkIn, checkOut);
+			if(erro != null) {
+				System.out.println("Erro ao reservar: "+erro);
+			}
+			else {
+				System.out.println("Reserva: "+reserva);
+			}
+		}
+		
+		sc.close();
+	}
 }
--- src/model/entities/Reservation.java
+++ src/model/entities/Reservation.java
@@ -1,5 +1,70 @@
 package model.entities;
 
-public class Reservation {
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.concurrent.TimeUnit;
 
+public class Reservation {
+	public int numeroQuarto;
+	public Date checkin;
+	public Date checkout;
+	
+	private static SimpleDateFormat sdf = new SimpleDateFormat("dd/MM/yyyy");
+	
+	public Reservation() {
+	}
+	
+	public Reservation(int numeroQuarto, Date checkin, Date checkout) {
+		this.numeroQuarto = numeroQuarto;
+		this.checkin = checkin;
+		this.checkout = checkout;
+	}
+
+	public int getNumeroQuarto() {
+		return numeroQuarto;
+	}
+
+	public void setNumeroQuarto(int numeroQuarto) {
+		this.numeroQuarto = numeroQuarto;
+	}
+
+	public Date getCheckin() {
+		return checkin;
+	}
+
+	public Date getCheckout() {
+		return checkout;
+	}
+	
+	public long duracao() {
+		long dif = checkout.getTime() - checkin.getTime();
+		return TimeUnit.DAYS.convert(dif, TimeUnit.MILLISECONDS);
+		
+	}
+	
+	public String atualizarDatas(Date checkin, Date checkout) {
+		Date agora = new Date();
+		if(checkin.before(agora) || checkout.before(agora)) {
+			return "A data de CheckIn ou CheckOut estão desatualizadas...";
+		}
+		if (!checkout.after(checkin)) {
+			return "A data de checkout deve ser posterior a de checkin...";
+		}
+		
+		this.checkin = checkin;
+		this.checkout = checkout;
+		return null;		
+	}
+	
+	@Override
+	public String toString() {
+		return "Quarto: "
+				+numeroQuarto
+				+", checkin:  "
+				+sdf.format(checkin)
+				+", checkout: "
+				+sdf.format(checkout)
+				+"duracao: " + duracao()
+				+" noites";
+	}
 }
